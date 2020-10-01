3f06cde07e70ec723b4a17cbfdb92a345fb63310
Claire Chang <c89650508@hotmail.com>
2020-10-01 00:03:51 -0500

    Updates

---
+++ precache-manifest.930ce7ea40d1ab6be1c623f84a05e9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae184188ba93b1406d43916f36b6c152",
+    "url": "/React-recipe-search/index.html"
+  },
+  {
+    "revision": "873caae2720597efb878",
+    "url": "/React-recipe-search/static/css/main.eeeb6a13.chunk.css"
+  },
+  {
+    "revision": "4f72925e60d19ca5665d",
+    "url": "/React-recipe-search/static/js/2.7f88d5e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-recipe-search/static/js/2.7f88d5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873caae2720597efb878",
+    "url": "/React-recipe-search/static/js/main.ca2d8fb9.chunk.js"
+  },
+  {
+    "revision": "fa77c80c7941ddc15a9d",
+    "url": "/React-recipe-search/static/js/runtime-main.bbf7e9fc.js"
+  }
+]);
